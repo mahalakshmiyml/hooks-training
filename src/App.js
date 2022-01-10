06d6f78b802590f5296bc96aab3819affbe26df2
mahalakshmiyml <lakshmiyml@gmail.com>
2022-01-10 16:02:28 +0530

    Codestarted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
+import UseState from './Components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h1> Hello Hooks</h1>
+        <UseState /> 
       </header>
     </div>
   );
---
+++ src/Components/UseState/UseState.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UseState = () => {
+    return (
+        <div>
+            <h1>UseState Hook</h1>
+            <buton></buton>
+        </div>
+    )
+}
+
+export default UseState
